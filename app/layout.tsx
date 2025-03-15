375ee3aadf1842d98e2b0831cd570515c8f96af1
Ala GARBAA <79337368+ala-garbaa-pro@users.noreply.github.com>
2025-03-15 22:49:31 +0100

    add dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className="dark"
+      style={{ colorScheme: "dark" }}
+    >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

6fc350ed4a2c387f69556a3252fdec52a3cf5865
Ala GARBAA <79337368+ala-garbaa-pro@users.noreply.github.com>
2025-03-16 10:08:19 +0100

    up

--- components/custom/lexical-editor/index.tsx
+++ components/custom/lexical-editor/index.tsx
@@ -1,9 +1,53 @@
 "use client";
 
-type LexicalRichTextEditorProps = {};
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 
+const theme = {
+  // Theme styling goes here
+  //...
+};
+
+function onError(error: Error) {
+  console.error(error);
+}
+
+function onChange(editorState: any) {
+  editorState.read(() => {
+    console.log(editorState);
+    // Read the contents of the EditorState here.
+    // This is where you would implement your custom save behavior.
+  });
+}
+
+type LexicalRichTextEditorProps = {};
 export function LexicalRichTextEditor({}: LexicalRichTextEditorProps) {
-  return <div>Hello</div>;
+  const initialConfig = {
+    namespace: "MyEditor",
+    theme,
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <RichTextPlugin
+        contentEditable={
+          <ContentEditable
+            aria-placeholder={"Enter some text..."}
+            placeholder={<div>Enter some text...</div>}
+          />
+        }
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+
+      <OnChangePlugin onChange={onChange} />
+      <HistoryPlugin />
+    </LexicalComposer>
+  );
 }
 
 export default LexicalRichTextEditor;
